b3ae95f05d5d78c2da9bbce7256428aec551ea9b
islamelhady <i.elhady44@gmail.com>
2020-08-21 19:40:10 +0200

    show toast for number of pages

--- app/src/main/java/com/elhady/cinema_movies/activity/MainActivity.java
+++ app/src/main/java/com/elhady/cinema_movies/activity/MainActivity.java
@@ -1,16 +1,31 @@
 package com.elhady.cinema_movies.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.elhady.cinema_movies.R;
+import com.elhady.cinema_movies.viewmodel.MostPopularTVShowViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MostPopularTVShowViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       viewModel = ViewModelProviders.of(this).get(MostPopularTVShowViewModel.class);
+        mostPopularTVShow();
+    }
+
+    private void mostPopularTVShow() {
+        viewModel.getMostPopularTVShow(0).observe(this,mostPopularTVshowResponse ->
+                Toast.makeText(getApplicationContext(), "Total Pages"+mostPopularTVshowResponse.getPages()
+                        , Toast.LENGTH_SHORT).show()
+        );
     }
 }
