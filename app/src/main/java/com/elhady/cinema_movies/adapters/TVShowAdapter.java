8995986f0fc90217a4518350595a802cd2992af9
islamelhady <i.elhady44@gmail.com>
2020-09-07 14:25:35 +0200

    add adapter

---
+++ app/src/main/java/com/elhady/cinema_movies/adapters/TVShowAdapter.java
@@ -0,0 +1,63 @@
+package com.elhady.cinema_movies.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.elhady.cinema_movies.DataBinderMapperImpl;
+import com.elhady.cinema_movies.R;
+import com.elhady.cinema_movies.databinding.ItemContainerTvShowBinding;
+import com.elhady.cinema_movies.models.TVShow;
+
+import java.util.List;
+
+public class TVShowAdapter extends RecyclerView.Adapter<TVShowAdapter.TVShowViewHolder> {
+    private List<TVShow> tvShows;
+    private LayoutInflater layoutInflater;
+
+    public TVShowAdapter(List<TVShow> tvShows) {
+        this.tvShows = tvShows;
+    }
+
+    @NonNull
+    @Override
+    public TVShowViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (layoutInflater == null) {
+            layoutInflater = LayoutInflater.from(parent.getContext());
+        }
+        ItemContainerTvShowBinding tvShowBinding = DataBindingUtil
+                .inflate(layoutInflater, R.layout.item_container_tv_show, parent, false);
+        return new TVShowViewHolder(tvShowBinding);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull TVShowViewHolder holder, int position) {
+        holder.bindTVShow(tvShows.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return tvShows.size();
+    }
+
+    class TVShowViewHolder extends RecyclerView.ViewHolder {
+
+        private ItemContainerTvShowBinding itemContainerTvShowBinding;
+
+        public TVShowViewHolder(@NonNull ItemContainerTvShowBinding itemContainerTvShowBinding) {
+            super(itemContainerTvShowBinding.getRoot());
+            this.itemContainerTvShowBinding = itemContainerTvShowBinding;
+        }
+
+        public void bindTVShow(TVShow tvShow) {
+            itemContainerTvShowBinding.setTvShow(tvShow);
+            itemContainerTvShowBinding.executePendingBindings();
+        }
+    }
+}
