d0600862a319a9ce1e09e06f6145547017d5607f
islamelhady <i.elhady44@gmail.com>
2020-09-08 02:12:31 +0200

    fix bugs in model tvshow

---
+++ app/src/main/java/com/elhady/tvshows/models/TVShow.java
@@ -0,0 +1,105 @@
+package com.elhady.tvshows.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class TVShow {
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("total")
+    private String total;
+
+    @SerializedName("permalink")
+    private String permalink;
+
+    @SerializedName("start_date")
+    private String start_date;
+
+    @SerializedName("country")
+    private String country;
+
+    @SerializedName("network")
+    private String network;
+
+    @SerializedName("status")
+    private String status;
+
+    @SerializedName("image_thumbnail_path")
+    private String image_thumbnail_path;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getPermalink() {
+        return permalink;
+    }
+
+    public void setPermalink(String permalink) {
+        this.permalink = permalink;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getNetwork() {
+        return network;
+    }
+
+    public void setNetwork(String network) {
+        this.network = network;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getImage_thumbnail_path() {
+        return image_thumbnail_path;
+    }
+
+    public void setImage_thumbnail_path(String image_thumbnail_path) {
+        this.image_thumbnail_path = image_thumbnail_path;
+    }
+}
