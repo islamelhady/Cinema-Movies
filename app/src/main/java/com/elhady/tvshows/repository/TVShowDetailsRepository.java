ac7812704743b1441b6b895e206136c88dc5c7e7
islamelhady <i.elhady44@gmail.com>
2020-09-08 04:11:09 +0200

    add repository of tv show details

---
+++ app/src/main/java/com/elhady/tvshows/repository/TVShowDetailsRepository.java
@@ -0,0 +1,39 @@
+package com.elhady.tvshows.repository;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.elhady.tvshows.models.TVShowDetailsResponse;
+import com.elhady.tvshows.network.ApiClient;
+import com.elhady.tvshows.network.ApiService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.http.Query;
+
+public class TVShowDetailsRepository {
+
+    private ApiService apiService;
+
+    public TVShowDetailsRepository() {
+        apiService = ApiClient.getRetrofit().create(ApiService.class);
+    }
+
+    public LiveData<TVShowDetailsResponse> getTVShowDetails(String tvShowId) {
+        MutableLiveData<TVShowDetailsResponse> data = new MutableLiveData<>();
+        apiService.getTVShowsDetails(tvShowId).enqueue(new Callback<TVShowDetailsResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<TVShowDetailsResponse> call, @NonNull Response<TVShowDetailsResponse> response) {
+                data.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<TVShowDetailsResponse> call, @NonNull Throwable t) {
+                data.setValue(null);
+            }
+        });
+        return data;
+    }
+}
