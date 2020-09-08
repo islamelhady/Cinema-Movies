c1fa66be2acbe9b187e805c8914de73074a10095
islamelhady <i.elhady44@gmail.com>
2020-09-08 03:58:38 +0200

    add api service tv show details

--- app/src/main/java/com/elhady/tvshows/network/ApiService.java
+++ app/src/main/java/com/elhady/tvshows/network/ApiService.java
@@ -1,5 +1,6 @@
 package com.elhady.tvshows.network;
 
+import com.elhady.tvshows.models.TVShowDetailsResponse;
 import com.elhady.tvshows.models.TVShowResponse;
 
 import retrofit2.Call;
@@ -9,4 +10,7 @@ import retrofit2.http.Query;
 public interface ApiService {
     @GET("most-popular")
     Call<TVShowResponse> getMostPopularTVShows(@Query("page") int page);
+    
+    @GET("show-details")
+    Call<TVShowDetailsResponse> getTVShowsDetails(@Query("q") String tvShowId);
 }
