04bbca4e91ceabbb2bb745347963712d407305d3
islamelhady <i.elhady44@gmail.com>
2020-09-08 04:31:29 +0200

    Add TV Show Details Activity

---
+++ app/src/main/java/com/elhady/tvshows/activity/TVShowDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.elhady.tvshows.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.elhady.tvshows.R;
+
+public class TVShowDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tv_show_details);
+    }
+}
