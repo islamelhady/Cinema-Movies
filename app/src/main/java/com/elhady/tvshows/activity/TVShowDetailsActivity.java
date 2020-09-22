362cd3f5346d82b1b49d5ecafbd1d2b8857b36e8
islamelhady <i.elhady44@gmail.com>
2020-09-22 05:20:50 +0200

    Edit Details Activity

--- app/src/main/java/com/elhady/tvshows/activity/TVShowDetailsActivity.java
+++ app/src/main/java/com/elhady/tvshows/activity/TVShowDetailsActivity.java
@@ -1,16 +1,43 @@
 package com.elhady.tvshows.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.elhady.tvshows.R;
+import com.elhady.tvshows.databinding.ActivityTvShowDetailsBinding;
+import com.elhady.tvshows.models.TVShowDetailsResponse;
+import com.elhady.tvshows.viewmodel.TVShowDetailsViewModel;
 
 public class TVShowDetailsActivity extends AppCompatActivity {
+
+    private ActivityTvShowDetailsBinding activityTvShowDetailsBinding;
+    private TVShowDetailsViewModel tvShowDetailsViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tv_show_details);
+        activityTvShowDetailsBinding = DataBindingUtil.setContentView(this, R.layout.activity_tv_show_details);
+
+        doInitialization();
+    }
+
+    private void doInitialization() {
+        tvShowDetailsViewModel = new ViewModelProvider(this).get(TVShowDetailsViewModel.class);
+        getTVShowDetails();
+    }
+
+    private void getTVShowDetails(){
+        activityTvShowDetailsBinding.setIsLoading(true);
+        String tvShowId = String.valueOf(getIntent().getIntExtra("id",-1));
+        tvShowDetailsViewModel.getTVShowDetails(tvShowId)
+                .observe(this, tvShowDetailsResponse -> {
+                    activityTvShowDetailsBinding.setIsLoading(false);
+                    Toast.makeText(getApplicationContext(), tvShowDetailsResponse.getTvShowDetails().getUrl(), Toast.LENGTH_SHORT).show();
+        });
     }
 }
