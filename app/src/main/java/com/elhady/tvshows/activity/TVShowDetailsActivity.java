39c3be137d1bb95ba91613b19ea4c93e82b46d7d
islamelhady <i.elhady44@gmail.com>
2020-09-22 07:28:37 +0200

    Add  Slider Indicators

--- app/src/main/java/com/elhady/tvshows/activity/TVShowDetailsActivity.java
+++ app/src/main/java/com/elhady/tvshows/activity/TVShowDetailsActivity.java
@@ -1,12 +1,18 @@
 package com.elhady.tvshows.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.elhady.tvshows.R;
@@ -33,22 +39,39 @@ public class TVShowDetailsActivity extends AppCompatActivity {
         getTVShowDetails();
     }
 
-    private void getTVShowDetails(){
+    private void getTVShowDetails() {
         activityTvShowDetailsBinding.setIsLoading(true);
-        String tvShowId = String.valueOf(getIntent().getIntExtra("id",-1));
+        String tvShowId = String.valueOf(getIntent().getIntExtra("id", -1));
         tvShowDetailsViewModel.getTVShowDetails(tvShowId)
                 .observe(this, tvShowDetailsResponse -> {
                     activityTvShowDetailsBinding.setIsLoading(false);
-                    if (tvShowDetailsResponse.getTvShowDetails().getPictures() != null){
+                    if (tvShowDetailsResponse.getTvShowDetails().getPictures() != null) {
                         loadImageSlider(tvShowDetailsResponse.getTvShowDetails().getPictures());
                     }
-        });
+                });
     }
 
-    private void loadImageSlider(String[] sliderImages){
+    private void loadImageSlider(String[] sliderImages) {
         activityTvShowDetailsBinding.sliderViewpager.setOffscreenPageLimit(1);
         activityTvShowDetailsBinding.sliderViewpager.setAdapter(new ImageSliderAdapter(sliderImages));
         activityTvShowDetailsBinding.sliderViewpager.setVisibility(View.VISIBLE);
         activityTvShowDetailsBinding.viewFadingEdge.setVisibility(View.VISIBLE);
+        setupSliderIndicators(sliderImages.length);
+    }
+
+    private void setupSliderIndicators(int count) {
+        ImageView[] indicators = new ImageView[count];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(8, 0, 8, 0);
+        for (int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext()
+                    , R.drawable.background_slide_indicator_inactive));
+            indicators[i].setLayoutParams(layoutParams);
+            activityTvShowDetailsBinding.layoutSliderIndicators.addView(indicators[i]);
+        }
+        activityTvShowDetailsBinding.layoutSliderIndicators.setVisibility(View.VISIBLE);
     }
 }
