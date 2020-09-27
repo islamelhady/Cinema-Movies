d46409ee6c3e837567088defde11c3350ffe2c20
islamelhady <i.elhady44@gmail.com>
2020-09-27 20:58:21 +0200

    Add TvShowDao class

---
+++ app/src/main/java/com/elhady/tvshows/dao/TvShowDao.java
@@ -0,0 +1,28 @@
+package com.elhady.tvshows.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.elhady.tvshows.models.TVShow;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+
+@Dao
+public interface TvShowDao {
+
+    @Query("SELECT * FROM tvShows")
+    Flowable<List<TVShow>> getWatchList();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Completable addWatchList(TVShow tvShow);
+
+    @Delete
+    void deleteFromWatchList(TVShow tvShow);
+
+}
