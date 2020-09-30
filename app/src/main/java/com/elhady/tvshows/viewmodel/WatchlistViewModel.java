5585f6811fd6e96ebba1eda4051a2d8ab9454118
islamelhady <i.elhady44@gmail.com>
2020-09-30 09:20:52 +0200

    Add Watchlist ViewModel class

---
+++ app/src/main/java/com/elhady/tvshows/viewmodel/WatchlistViewModel.java
@@ -0,0 +1,27 @@
+package com.elhady.tvshows.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.elhady.tvshows.database.TvShowDatabase;
+import com.elhady.tvshows.models.TVShow;
+
+import java.util.List;
+
+import io.reactivex.Flowable;
+
+public class WatchlistViewModel extends AndroidViewModel {
+
+    private TvShowDatabase tvShowDatabase;
+
+    public WatchlistViewModel(@NonNull Application application) {
+        super(application);
+        this.tvShowDatabase = TvShowDatabase.getTvShowDatabase(application);
+    }
+
+    public Flowable<List<TVShow>> loadWatchList(){
+        return tvShowDatabase.tvShowDao().getWatchList();
+    }
+}
