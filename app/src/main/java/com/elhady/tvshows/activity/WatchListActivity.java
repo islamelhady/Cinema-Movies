b5cd70885c3cef5d2d479047a65c15ec1739c835
islamelhady <i.elhady44@gmail.com>
2020-09-30 09:12:56 +0200

    Add activity_watch_list.xml

---
+++ app/src/main/java/com/elhady/tvshows/activity/WatchListActivity.java
@@ -0,0 +1,16 @@
+package com.elhady.tvshows.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.elhady.tvshows.R;
+
+public class WatchListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch_list);
+    }
+}
