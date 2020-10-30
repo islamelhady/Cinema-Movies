f8101e05e38ebeb0146317ba630804ba7f977022
islamelhady <i.elhady44@gmail.com>
2020-10-30 14:44:06 +0200

    Add Watchlist activity class

--- app/src/main/java/com/elhady/tvshows/activity/WatchListActivity.java
+++ app/src/main/java/com/elhady/tvshows/activity/WatchListActivity.java
@@ -1,16 +1,51 @@
 package com.elhady.tvshows.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.elhady.tvshows.R;
+import com.elhady.tvshows.databinding.ActivityWatchListBinding;
+import com.elhady.tvshows.viewmodel.WatchlistViewModel;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class WatchListActivity extends AppCompatActivity {
+
+    private ActivityWatchListBinding binding;
+    private WatchlistViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_watch_list);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_watch_list);
+        doInitialization();
+    }
+
+    private void doInitialization() {
+        viewModel = new ViewModelProvider(this).get(WatchlistViewModel.class);
+        binding.imageBack.setOnClickListener(view -> onBackPressed());
+    }
+
+    private void loadWatchList() {
+        binding.setIsLoading(true);
+        CompositeDisposable compositeDisposable = new CompositeDisposable();
+        compositeDisposable.add(viewModel.loadWatchList().subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(tvShows -> {
+            binding.setIsLoading(false);
+                    Toast.makeText(getApplicationContext(), "Watchlist: " + tvShows.size(), Toast.LENGTH_SHORT).show();
+                }));
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        loadWatchList();
     }
 }
